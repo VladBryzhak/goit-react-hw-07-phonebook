b4e6f2dc012c98b0b19276f728140800cc5c105c
VladBryzhak <124492057+VladBryzhak@users.noreply.github.com>
2024-02-19 20:27:30 +0200

    finale-newDesign

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import React, { useEffect } from 'react';
+import ContactForm from './ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../redux/operations';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.contacts.isLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {isLoading ? <p>Loading...</p> : <Filter />}
+      <ContactList />
     </div>
   );
 };
